7758a8229ae6d230cb3056c580ffc1bce4d0f86f
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-09-09 19:59:01 +0700

    исправления

--- users/models.py
+++ users/models.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import NULLABLE
+
 
 class User(AbstractUser):
     username = None
 
     USERNAME_FIELD = 'email'
-    avatar = models.ImageField(upload_to="media/")
-    email = models.EmailField(unique=True, verbose_name='Почта', null=False, blank=False)
+    avatar = models.ImageField(upload_to="media/", **NULLABLE,verbose_name='аватар пользователя')
+    email = models.EmailField(unique=True, verbose_name='почта', null=False, blank=False)
     phone = models.CharField(max_length=11, verbose_name='телефон', null=True, blank=True)
     city = models.CharField(max_length=30, verbose_name='город', null=True, blank=True)
-    token = models.CharField(max_length=255, verbose_name="Токен", blank=True, null=True)
 
     REQUIRED_FIELDS = []
 
