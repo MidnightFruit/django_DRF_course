3a8f438540bfea06067077f57cb97e01ed35c46d
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-09-09 23:21:22 +0700

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
@@ -22,3 +23,4 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='user')),
     path('material/', include('materials.urls', namespace='materials')),
 ]
+urlpatterns += staticfiles_urlpatterns()
