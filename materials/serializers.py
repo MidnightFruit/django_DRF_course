0583713ab2e85331e7efab50645274795d3f228e
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-09-09 23:55:10 +0700

    ДЗ 24.2

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,13 +3,20 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(read_only=True, many=True)
+
+    def get_lessons_count(self, obj):
+        return Lesson.objects.filter(course=obj).count()
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
